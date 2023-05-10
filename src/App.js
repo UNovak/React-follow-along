d23112d21bbd62ab92edb32d8940a0e70b132bc1
UNovak <urbanwritescode@gmail.com>
2023-05-10 22:32:38 +0200

    current page: 36
    created an event handler for the onChange event.
    learned how to use props to pass arguments to functions

--- src/App.js
+++ src/App.js
@@ -1,57 +1,44 @@
 import React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const list2 = [
-  {
-    title: "React1",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux1",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+  // do something
+  const handler = (event) => {
+    console.log(event.target.value);
+  };
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-    <hr />
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handler} />
+      <hr />
+      <List list={stories} />
+      <List list={stories} />
+    </div>
+  );
+};
 
-    <List list />
-    <List list2 />
-  </div>
-);
-
-const List = () => {
-  return list.map((item) => (
+const List = (props) =>
+  props.list.map((item) => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -61,7 +48,6 @@ const List = () => {
       <span>{item.points}</span>
     </div>
   ));
-};
 
 if (module.hot) {
   module.hot.accept();
